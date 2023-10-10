e3ee7863fee6d513dc3cb577475ca7d03d017cc9
maneesh756 <114976480+maneesh756@users.noreply.github.com>
2023-10-10 20:14:13 +0530

    Create sin(x)_by_taylor_formula.cpp

---
+++ sin(x)_by_taylor_formula.cpp
@@ -0,0 +1,41 @@
+/***********Calculate sin(x) using taylor series
+expansion
+
+sin(x)\approx\sum_{n=0}^a (-1)^n
+\frac{x^{(2n+1)}{(2n+1)!}
+Here `a’ is a large number taken as user
+input.************/
+
+#include<iostream>
+using namespace std;
+#include<cmath>
+#include<stdlib.h>
+int fact(int expo)
+{
+        int i;
+        float fact=1.0;
+        for(i=0;i<=expo;i++)
+        {
+                fact =fact *1;
+        }
+        return(fact);
+}
+int main()
+{
+        int i,n;
+        float x,q,sum,t;
+        cout<<"enter the value of x :";
+        cin>>x;
+        cout<<"enter the value for the number of terms:";
+        cin>>n;
+        q=x*3.14159/180;
+        t=q;
+        sum=q;
+        int expo=2*n+1;
+         for(i=0;i<=n;i++)
+        {
+                t=t*(pow(-1,n)*pow(q,expo)/fact(expo));
+                sum=sum+t;
+        }
+        cout<<"the value of the sin("<<x<<") =" <<sum<<"\n";
+}
